b6001c551286acd278e7da5ff0d956254a59e9b7
dvirbs10 <dvir.bensimhon@mail.huji.ac.il>
2021-12-14 15:29:58 +0200

    After passing the tests

--- board.py
+++ board.py
@@ -1,23 +1,18 @@
 import typing
-import car
-import json
-import helper
 from typing import *
-
+import car
 
 class Board:
     """
     The Board of the game
     """
-    DEST = (3, 7)
 
     def __init__(self):
         # implement your code and erase the "pass"
         # Note that this function is required in your Board implementation.
         # However, is not part of the API for general board types.
-        self.__cars: typing.Dict = helper.load_json('car_config.json')  # List of car the add after checking the conditions for add car in the methead add_car
-                                    # TODO- need to check if the car name is correct and lengh and everyting
-        self.__board_list: typing.List = self.__board_initial()
+        self.__cars: List[car] = []
+
 
     def __str__(self):
         """
@@ -26,50 +21,37 @@ class Board:
         """
         # The game may assume this function returns a reasonable representation
         # of the board for printing, but may not assume details about it.
-        current_stat_str = ''
-        board = self.__board_list
-        for row_i, row in enumerate(board):
-            if row_i > 0:
-                current_stat_str += '\n'
-            for col in row:
-                #TODO need to change when i will know what object will be in the board
-                current_stat_str += str(col)
-        return current_stat_str
-
-    def __board_initial(self):
-        """
-        initial the board
-        :return: 2D lists with exit in (3,7)
-        """
         board = list()
         for row in range(7):
             board.append(list())
             for column in range(7):
-                board[row].append((row, column))
-        board[3].append(self.DEST)
-        return board
+                board[row].append('_')
+        board[3].append('E')
 
-    def cell_list(self):
+        for car in self.__cars:
+            for cor in car.car_coordinates():
+                board[cor[0]][cor[1]] = car.get_name()
+
+        current_stat_str = ''
+        for row_i, row in enumerate(board):
+            if row_i > 0:
+                current_stat_str += '\n'
+            for col in row:
+                current_stat_str += col
+        return current_stat_str
+
+    def cell_list(self) -> List:
         """ This function returns the coordinates of cells in this board
         :return: list of coordinates
         """
         # In this board, returns a list containing the cells in the square
         # from (0,0) to (6,6) and the target cell (3,7)
-        # for row_i, row in enumerate(self.board_initial):
-        return self.__board_list
-
-    def all_cars_coordinate(self) -> typing.List[typing.Tuple]:
-        """
-        getting all coordinates of the cars in board
-        :return: list of tuple
-        """
-        cars = self.__cars
-        all_cars_cord = list()
-        for car in cars:
-            all_cars_cord += car.car_coordinates()
-        return all_cars_cord
-
-
+        cell_list = list()
+        for row in range(7):
+            for column in range(7):
+                cell_list.append((row, column))
+        cell_list.append(self.target_location())
+        return cell_list
 
     def possible_moves(self) -> List[Tuple]:
         """ This function returns the legal moves of all cars in this board
@@ -78,24 +60,15 @@ class Board:
         """
         # From the provided example car_config.json file, the return value could be
         # [('O','d',"some description"),('R','r',"some description"),('O','u',"some description")]
-        cars = self.__cars
-        board_possible_moves = list()
-        cars_positions = self.all_cars_coordinate()
-                        # TODO - need to check what is the object car in the sef.__cars
-        if len(cars) == 0:
-            return board_possible_moves
-        else:
-            # TODO maybe can write the next loops in smarter way
-            for car in cars.keys():
-                car_possible_moves: typing.Dict = car.possible_moves()  # {'d': "cause the...."}
-                for possible_move in car_possible_moves.keys():
-                    empty_cell_needed: List[Tuple] = car.movement_requirements(possible_move)  # car in locations [(1,2),(2,2)] requires [(3,2)]
-                    if empty_cell_needed in self.cell_list() and empty_cell_needed not in cars_positions:
-                        board_possible_moves.append((car.get_name(), possible_move, 'do what feels you good'))
-                    
-
-
-
+        board_possible_moves = []
+        for car in self.__cars:
+            car_possible_moves: typing.Dict = car.possible_moves()  # {'d': "cause the...."}
+            for possible_move, description in car_possible_moves.items():
+                empty_cell_needed: Tuple = car.movement_requirements(possible_move)[
+                    0]  # car in locations [(1,2),(2,2)] requires [(3,2)]
+                if empty_cell_needed in self.cell_list() and not self.cell_content(empty_cell_needed):
+                    board_possible_moves.append((car.get_name(), possible_move, description))
+        return board_possible_moves
 
     def target_location(self):
         """
@@ -103,7 +76,7 @@ class Board:
         :return: (row,col) of goal location
         """
         # In this board, returns (3,7)
-        return self.DEST
+        return 3, 7
 
     def cell_content(self, coordinate: typing.Tuple):
         """
@@ -111,9 +84,10 @@ class Board:
         :param coordinate: tuple of (row,col) of the coordinate to check
         :return: The name if the car in coordinate, None if empty
         """
-        # implement your code and erase the "pass"
-        # ??? to check what happed if the cordinate not in the board ???
-        pass
+        for car in self.__cars:
+            if coordinate in car.car_coordinates():
+                return car.get_name()
+        return
 
     def add_car(self, car):
         """
@@ -124,7 +98,15 @@ class Board:
         # Remember to consider all the reasons adding a car can fail.
         # You may assume the car is a legal car object following the API.
         # implement your code and erase the "pass"
-        pass
+        for board_car in self.__cars:
+            if car.get_name() == board_car.get_name():
+                return False
+
+        for coordinate in car.car_coordinates():
+            if self.cell_content(coordinate) or (coordinate not in self.cell_list()):
+                return False
+        self.__cars.append(car)
+        return True
 
     def move_car(self, name, movekey):
         """
@@ -134,9 +116,20 @@ class Board:
         :return: True upon success, False otherwise
         """
         # implement your code and erase the "pass"
-        pass
+        #self.possible_moves-[('O','d',"some description"),('R','r',"some description"),('O','u',"some description")]
+
+        for car_description in self.possible_moves():
+            if name == car_description[0] and movekey == car_description[1]:
+                for car in self.__cars:
+                    if name == car.get_name():
+                        return car.move(movekey)
+        return False
 
 
-if __name__ == '__main__':
-    my_board = Board()
-    print(my_board)
+
+
+
+
+# if __name__ == '__main__':
+#     my_board = Board()
+#     print(my_board)
