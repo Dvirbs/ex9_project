5ab8a04e9d9b8b8b6da9fe6ab642080851ca7587
dvirbs10 <dvir.bensimhon@mail.huji.ac.il>
2021-12-15 18:23:24 +0200

    AFTER LAB SUPPORT

--- game.py
+++ game.py
@@ -18,6 +18,7 @@ class Game:
         # You may assume board follows the API
         # implement your code here (and then delete the next line - 'pass')
         self.__board = board
+        self.player_quit = False
 
     def __legal_car_name(self, name: str):
         """
@@ -34,7 +35,7 @@ class Game:
         :param direction: the direction of the move
         :return: True it is legal
         """
-        if direction != ('u' or 'd' or 'l' or 'r'):
+        if direction != 'l' and direction != 'r' and direction != 'u' and direction != 'd':
             return False
         for possible_move in self.__board.possible_moves():  # [('O','d',"some description"),('R','r',"some description")]
             possible_name = possible_move[0]
@@ -61,34 +62,38 @@ class Game:
         don't interfere with the API.
         """
         # implement your code here (and then delete the next line - 'pass')
-        print(self.__board)
         inp = str(input('\n*** write the car color and direction that you wont to move : *** '))
-        if len(inp)<3:
+        if inp == '!':
+            self.player_quit = True
+        while len(inp) != 3 or inp[1] != ',':
             inp = str(input('\n*** input not correct!(for Yellow car move down type --> Y,d) *** '))
         car_name = inp[0]
         move_direction = inp[2]
-        while not (self.__legal_car_name(car_name) or self.__legel_car_move(car_name, move_direction)):
+        while not (self.__legal_car_name(car_name) and self.__legel_car_move(car_name, move_direction)):
             inp = str(input('\n*** input not correct!(for Yellow car move down type --> Y,d) *** '))
             car_name = inp[0]
             move_direction = inp[2]
         self.__board.move_car(car_name, move_direction)
         print('\n', self.__board)
 
-    def __not_game_ended(self):
+    def __game_ended(self):
         """
         check if the game not ended
         :return: True if the game not ended
         """
         target_location = self.__board.target_location()
-        if target_location != '!':
+        if self.player_quit:
             return True
+        # if target_location != '!':
+        #     return True
         target_content = self.__board.cell_content(target_location)
         if target_content is not None:
-            car_name = target_content
-            info = helper.load_json('car_config.json')[car_name]
-            orientation = info[2]
-            if orientation == self.VERTICAL:
-                return True
+            return True
+            # car_name = target_content
+            # info = helper.load_json('car_config.json')[car_name]
+            # orientation = info[2]
+            # if orientation == self.VERTICAL:
+            #     return True
         return False
 
     def play(self):
@@ -97,8 +102,10 @@ class Game:
         :return: None
         """
         # implement your code here (and then delete the next line - 'pass')
-        while self.__not_game_ended():
+        print('\n', self.__board)
+        while not self.__game_ended():
             self.__single_turn()
+        print("Game Over")
 
 
 def not_legal_car(name: str, length: int, orientation: int):
@@ -108,7 +115,9 @@ def not_legal_car(name: str, length: int, orientation: int):
     """
     if (len(name) != 1) or (type(name) != str) or name.islower():
         return True
-    elif (not 2 <= length <= 4) or (orientation != (1 or 0)):
+    if orientation != 0 and orientation != 1:
+        return True
+    elif length > 4 or length < 2:
         return True
     else:
         return False
@@ -132,4 +141,3 @@ if __name__ == "__main__":
         game_board.add_car(the_car)
     game = Game(game_board)
     game.play()
-
